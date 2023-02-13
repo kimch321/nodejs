0659b11ee52f5c2dc5cb1ef88de11ce2dd942486
KIM CHUNG IL <cnddlf12@naver.com>
2023-02-13 11:41:58 +0900

    data.go.kr - 코로나 확진자 현황(xml)

--- crwaling05.js
+++ crwaling05.js
@@ -5,36 +5,45 @@
 // 사용할 패키지 가져오기 : require(패키지명)
 const axios = require('axios');
 const cheerio = require('cheerio');
+const {XMLParser} = require('fast-xml-parser');
 
 async function main () {        // 비동기 I/O 지원 함수 정의
 
 // 접속할 url 지정, 쿼리스트링, user-agent 헤더 지정
+    // apiType : xml 또는 JSON
     const URL = 'http://apis.data.go.kr/1352000/ODMS_COVID_04/callCovid04Api';
     const params = {
         'serviceKey':'CCjWE3KHmoW3oJhjbFufhSELDDm/9OSuQlW+ql/u8LA+IBXh3pG9PuG5+14ElD581dEaVkxVhbV0/IAK9fsfpQ==',
-        'apiType':'JSON',
-        'gubun':'서울',
-        'numOfRows':1000
+        'apiType':'xml',
+        //'pageNo': 1,
+        //'numOfRows':500,
+        'std_day':'2023-02-12',
+        'gubun':''
     };
     const headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36 Edg/109.0.1518.78'}
 
     // axios로 접속해서 대기오염정보를 받아옴
-    const json = await axios.get(URL, {
+    const xml = await axios.get(URL, {
         params : params,
         headers: headers
     }); // 서버 요청시 User-Agent 헤더 사용
 
     // 받아온 데이터 확인
-     console.log(json.data);
+    // console.log(xml.data);
+
+    // JSON으로 변환하기
+    const parser = new XMLParser();
+    let json = parser.parse(xml.data)
+    // console.log(json);
 
     // JSON으로 불러오기
-    let items = json.data['items']
-     console.log(items)
+    let items = json['response']['body']['items']['item']
+    console.log(items)
+    //console.log(items[5]);
 
     // 미세먼지 정보 출력
-    // pm25Value는 출력 안됨!!
-    // for(let item of items) {
-    //     console.log(item.sidoName,item.stationName,item.pm10Value,item.pm25Grade,item.dataTime);
-    // }
+    for(let item of items) {
+         console.log(`지역 : ${item.gubun}, 전일 확인자수:${item.incDec},총 확진자수 ${item.defCnt},사망자수 ${item.deathCnt},측정일: ${item.stdDay} `);
+    }
 }
 main();
