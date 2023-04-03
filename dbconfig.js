9ac6b9b87c2e917061c0d3ffdfd5afd67f7be93c
KIM CHUNG IL <cnddlf12@naver.com>
2023-04-03 18:20:26 +0900

    연습

--- dbconfig.js
+++ dbconfig.js
@@ -1,6 +1,6 @@
 module.exports =
 {
     user : 'bigdata',
-    password: '-',
-    connectionString: '-'
+    password: 'bigdata',
+    connectionString: '54.180.140.184:1521/XE'
 }
---
+++ dbconfig2.js
@@ -0,0 +1,6 @@
+module.exports = {
+    host :'fullstacks.csgna22pwwig.ap-northeast-2.rds.amazonaws.com',
+    user:'admin',
+    password:'fullstack_2023',
+    database:'fullstacks'
+}
